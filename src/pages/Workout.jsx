a65b15134c32abec4d1590f5d50b2d2ddab76136
Rohit <rohitkhatri111112@gmail.com>
2024-07-21 00:12:24 +0530

    commit

--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -1,30 +1,94 @@
-/* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react";
-import { PuffLoader } from 'react-spinners'
-import useFetch from "../hooks/useFetch";
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { PuffLoader } from 'react-spinners';
+// import useFetch from "../hooks/useFetch";
 
 const Workout = () => {
-
     const [selectedTab, setSelectedTab] = useState("Workout Plans");
+    // const { data, loading, error, setURL } = useFetch("https://youtube.googleapis.com/youtube/v3/search?q=" + encodeURIComponent(selectedTab) + "&part=snippet&type=video&maxResults=20&videoEmbeddable=true&key=AIzaSyBMFhsPQrVsj7PVtc1kpVVUrLGam0gZRoo");
+    const [URL, setURL] = useState("https://youtube.googleapis.com/youtube/v3/search?q=" + encodeURIComponent(selectedTab) + "&part=snippet&type=video&maxResults=20&videoEmbeddable=true&key=AIzaSyBMFhsPQrVsj7PVtc1kpVVUrLGam0gZRoo")
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+    const [data, setData] = useState([])
 
 
-    const { data, loading, error, setURL } = useFetch("https://youtube.googleapis.com/youtube/v3/search?q=" + encodeURIComponent(selectedTab) + "&part=snippet&type=video&maxResults=20&videoEmbeddable=true&key=AIzaSyBMFhsPQrVsj7PVtc1kpVVUrLGam0gZRoo") // Custom Hook
+    useEffect(() => {
+        (async () => {
+            try {
+                setError(false)
+                setLoading(true)
+                const resp = await axios.get(URL);
+                const result = resp.data
+                setData(result.items)
+            } catch (error) {
+                console.log("Error");
+                setError(true)
+            } finally {
+                setLoading(false)
+            }
+        })()
+    }, [selectedTab, URL])
+
+    const playerRef = useRef(null);
+    const [currentVideoId, setCurrentVideoId] = useState(null);
+
+    useEffect(() => {
+        // Load the YouTube Iframe API script
+        const tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/iframe_api";
+        const firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        // Initialize the YouTube player
+        window.onYouTubeIframeAPIReady = () => {
+            playerRef.current = new window.YT.Player('youtube-player', {
+                height: '300',
+                width: '100%',
+                videoId: currentVideoId,
+                events: {
+                    onReady: onPlayerReady,
+                    onStateChange: onPlayerStateChange,
+                },
+            });
+        };
+
+        return () => {
+            // Cleanup the player instance
+            if (playerRef.current) {
+                playerRef.current.destroy();
+            }
+        };
+    }, [currentVideoId]);
+
+    const onPlayerReady = (event) => {
+        // Player is ready
+    };
+
+    const onPlayerStateChange = (event) => {
+        // Handle player state changes
+    };
 
     const handleTabSelect = (tab) => {
         setSelectedTab(tab);
-        setURL("https://utube.googleapis.com/youtube/v3/search?q=" + encodeURIComponent(tab) + "&part=snippet&type=video&maxResults=25&videoEmbeddable=true&key=AIzaSyBMFhsPQrVsj7PVtc1kpVVUrLGam0gZRoo");
+        setURL("https://youtube.googleapis.com/youtube/v3/search?q=" + encodeURIComponent(tab) + "&part=snippet&type=video&maxResults=25&videoEmbeddable=true&key=AIzaSyBMFhsPQrVsj7PVtc1kpVVUrLGam0gZRoo");
     };
-    console.log(data);
+
+    // const handleVideoSelect = (videoId) => {
+    //     setCurrentVideoId(videoId);
+    //     if (playerRef.current) {
+    //         playerRef.current.loadVideoById(videoId);
+    //     }
+    // };
 
     if (error) {
-        return <p className="text-3xl text-center font-semibold">Something Went Wrong</p>
+        return <p className="text-3xl text-center font-semibold h-screen">Something Went Wrong</p>;
     }
 
     return (
         <>
-            <div className="md:container mx-auto md:p-2 p-5">
+            <div className={`md:container mx-auto md:p-2 p-5 ${loading ? `h-screen` : ``}`}>
                 <div className="my-3">
-                    <p className="text-2xl mx-4 font-medium">Workouts</p>
+                    <p className="text-2xl mx-4 font-medium">Workouts Videos</p>
                 </div>
                 <div className="mx-auto flex justify-around items-center w-[90%]">
                     <div className="flex justify-center mb-4 border-b-4">
@@ -40,44 +104,67 @@ const Workout = () => {
                                     ? "border-blue-500 border-b-4 font-bold text-black"
                                     : "text-gray-500 border-b-4 border-gray-200 hover:text-gray-700"
                                     }`}
-
                                 onClick={() => handleTabSelect(tab.title)}
                             >
                                 {tab.title}
                             </p>
                         ))}
                     </div>
-
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
                     {loading ?
-                        (<PuffLoader
-                            color="skyblue"
-                            cssOverride={{}}
-                            loading={loading}
-                            size={50}
-                            speedMultiplier={2}
-                        />)
+                        (
+                            <div className="mx-auto flex justify-center items-center">
+                                <PuffLoader
+                                    color="skyblue"
+                                    className="mx-auto "
+                                    loading={loading}
+                                    size={50}
+                                    speedMultiplier={3}
+                                />
+                            </div>
+                        )
                         : data.map((video, index) => {
                             // Extracting and truncating the video title
                             const title = video.snippet.title.substring(0, 40) + " ...";
+                            const videoId = video.id.videoId;
 
                             return (
                                 <div key={index} className="p-2">
-                                    <div className="">
+                                    <div className="" >
                                         <iframe
                                             className="w-full h-[300px]"
-                                            src={`https://www.youtube.com/embed/${video.id.videoId}?rel=0&modesbranding=1`}>
-                                        </iframe>
+                                            src={`https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=1`}
+                                            frameBorder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                            allowFullScreen
+                                        ></iframe>
                                     </div>
                                     <p className="text-xl font-semibold mt-2">{title}</p>
                                 </div>
                             );
                         })}
                 </div>
+                {currentVideoId && (
+                    <div className="my-4">
+                        <iframe
+                            className="w-full h-[300px]"
+                            src={`https://www.youtube.com/embed/${currentVideoId}?rel=0&modestbranding=1`}
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+                )}
+                {/* <div id="youtube-player"></div>
+                <div className="flex justify-center mt-4">
+                    <button onClick={() => playerRef.current && playerRef.current.playVideo()} className="px-4 py-2 bg-blue-500 text-white rounded">Play</button>
+                    <button onClick={() => playerRef.current && playerRef.current.pauseVideo()} className="px-4 py-2 bg-blue-500 text-white rounded">Pause</button>
+                    <button onClick={() => playerRef.current && playerRef.current.stopVideo()} className="px-4 py-2 bg-blue-500 text-white rounded">Stop</button>
+                </div> */}
             </div>
         </>
-    )
+    );
 };
 
 export default Workout;
